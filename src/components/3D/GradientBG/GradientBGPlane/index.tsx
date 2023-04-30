70bf2736a9a31f32fc3fc28221e2bedd0fe32cc1
Balraj Johal <balraj.johal@phntms.com>
2023-04-30 16:08:00 +0100

    setup shader material

--- src/components/3D/GradientBG/GradientBGPlane/index.tsx
+++ src/components/3D/GradientBG/GradientBGPlane/index.tsx
@@ -3,12 +3,33 @@
 import { Plane } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 
+const vertexShader = `
+varying vec3 vUv; 
+
+void main() {
+  vUv = position; 
+
+  vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+  gl_Position = projectionMatrix * modelViewPosition; 
+}
+`;
+const fragmentShader = `
+varying vec3 vUv;
+
+void main() {
+  gl_FragColor = vec4(vec3(0.5), 1.0);
+}
+`;
+
 const GradientBGPlane = () => {
   const { viewport } = useThree();
 
   return (
     <Plane args={[viewport.width, viewport.height]}>
-      <meshBasicMaterial color="hotpink" />
+      <shaderMaterial
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+      />
     </Plane>
   );
 };
