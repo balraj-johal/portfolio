b060d1c726b84cdb28bd3f6654b4309004dd4066
Balraj Johal <balraj.johal@phntms.com>
2023-05-10 20:20:18 +0100

    Setup fonts

--- src/components/UI/RootBody/styles.ts
+++ src/components/UI/RootBody/styles.ts
@@ -1,13 +1,20 @@
 import { styled } from "@phntms/css-components";
-import { Anton } from "next/font/google";
+import { Anton, Abel } from "next/font/google";
 import css from "./style.module.css";
 
 // If loading a variable font, you don't need to specify the font weight
-const anton = Anton({
+const ANTON = Anton({
   weight: "400",
-  subsets: ["latin"]
+  subsets: ["latin"],
+  variable: '--font-anton'
+})
+
+const ABEL = Abel({
+  weight: "400",
+  subsets: ["latin"],
+  variable: '--font-abel'
 })
 
 export const RootBodyElement = styled('body', {
-  css: [css.RootBodyElement, anton.className],
+  css: [css.RootBodyElement, ANTON.variable, ABEL.variable],
 })
