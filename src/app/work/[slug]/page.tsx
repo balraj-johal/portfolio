9de58dc06611da94dc6d0e580a8552ddf7cd7217
Balraj Johal <balraj.johal@phntms.com>
2023-05-21 13:35:02 +0100

    Add 404 for dynamic route

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -1,11 +1,28 @@
+import { PROFRESSIONAL_ENTRIES } from "@/content/profressional";
+import { notFound } from "next/navigation";
+
 interface Props {
   params: {
     slug: string;
   };
 }
 
-export default function Home({ params }: Props) {
+export async function generateStaticParams() {
+  return PROFRESSIONAL_ENTRIES.map((entry) => ({
+    slug: entry.slug,
+  }));
+}
+
+const isRouteValid = (slug: string) => {
+  for (const entry of PROFRESSIONAL_ENTRIES) {
+    if (entry.slug === slug) return true;
+  }
+  return false;
+};
+
+export default function Work({ params }: Props) {
   const { slug } = params;
+  if (!isRouteValid(slug)) notFound();
 
   return <h1>{slug}</h1>;
 }
--- src/content/profressional.ts
+++ src/content/profressional.ts
@@ -3,35 +3,35 @@ import { ContentEntry } from "@/types/content";
 
 export const PROFRESSIONAL_ENTRIES: ContentEntry[] = [
   {
-    slug: "Festive Fiends",
+    slug: "festive-fiends",
     title: "Festive Fiends",
     oneLiner: "FWA Site of the Day",
     imageID: "festive_fiends",
     href: "https://www.festivefiends.com/",
   },
   {
-    slug: "Google's Digital Sovereignty",
+    slug: "google-digsov",
     title: "Google's Digital Sovereignty",
     oneLiner: "Filler filler filler filler",
     imageID: "festive_fiends",
     href: "https://www.cloudsovereignty.withgoogle.com",
   },
   {
-    slug: "Google's Shopper Hopper",
+    slug: "google-shopper-hopper",
     title: "Google's Shopper Hopper",
     oneLiner: "Filler filler filler filler",
     imageID: "festive_fiends",
     href: "https://www.shopperhopper.withgoogle.com",
   },
   {
-    slug: "Zendesk 15 Years",
+    slug: "zendesk-15-years",
     title: "Zendesk 15 Years",
     oneLiner: "Filler filler filler filler",
     imageID: "festive_fiends",
     href: "",
   },
   {
-    slug: "Trustpilot Meme Generator",
+    slug: "trustpilot-memes",
     title: "Trustpilot Meme Generator",
     oneLiner: "Filler filler filler filler",
     imageID: "festive_fiends",
