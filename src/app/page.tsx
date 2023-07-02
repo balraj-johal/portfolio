c0a225753f8af049aa96844b03c065afd8ab08d0
Balraj Johal <balrajsjohal@gmail.com>
2023-07-02 21:12:43 +0100

    test if this breaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { getContent } from "@/content/contentful";
 import Title from "@/components/UI/Title";
 import Subtitle from "@/components/UI/Subtitle";
 import MyWork from "@/components/UI/MyWork";
 import Main from "@/components/UI/Main";
 import Hero from "@/components/UI/Hero";
 
-export default function Home() {
+export default async function Home() {
+  const content = await getContent("professionalWork");
+  // console.log(content);
+
   return (
     <Main>
       <Hero>
