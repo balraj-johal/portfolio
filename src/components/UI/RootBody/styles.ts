5ee83f7eade95c965924dceda2df8d53fd4f2b5e
Balraj Johal <balraj-johal@users.noreply.github.com>
2023-08-01 00:10:39 +0100

    Feat/prep new design (#22)
    * Fix up colours
    
    * Install fonts
    
    * Tweak custom cursor a tad
    
    * Fix linter errs

--- src/components/UI/RootBody/styles.ts
+++ src/components/UI/RootBody/styles.ts
@@ -1,21 +1,9 @@
-import { Anton, Abel } from "next/font/google";
 import { styled } from "@phntms/css-components";
+
+import { FONT_CLASSES } from "@/theme/fonts/fonts";
 
 import css from "./style.module.css";
 
-// If transitioning a variable font, you don't need to specify the font weight
-const ANTON = Anton({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-anton",
-});
-
-const ABEL = Abel({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-abel",
-});
-
 export const RootBodyElement = styled("body", {
-  css: [css.RootBodyElement, ANTON.variable, ABEL.variable],
+  css: [css.RootBodyElement, FONT_CLASSES],
 });
