4440ca8252519db02897002a8c23c8e3f72217f1
Balraj <balraj.johal@phntms.com>
2023-07-31 23:55:24 +0100

    Install fonts

---
+++ src/theme/fonts/fonts.ts
@@ -0,0 +1,37 @@
+import localFont from "next/font/local";
+
+const supplySans = localFont({
+  src: [
+    {
+      path: "./PPSupplySans-Regular.otf",
+      weight: "200 400",
+      style: "normal",
+    },
+    {
+      path: "./PPSupplySans-Ultralight.otf",
+      weight: "200",
+      style: "light",
+    },
+  ],
+  variable: "--font-supply-sans",
+});
+
+const supplyMono = localFont({
+  src: [
+    {
+      path: "./PPSupplyMono-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./PPSupplySans-Ultralight.otf",
+      weight: "200",
+      style: "light",
+    },
+  ],
+  variable: "--font-supply-mono",
+});
+
+export const FONT_CLASSES = [supplySans.variable, supplyMono.variable].join(
+  " "
+);
