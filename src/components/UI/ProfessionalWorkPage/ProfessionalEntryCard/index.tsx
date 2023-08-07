50a486afdb616b093c4bf56aab9ab57057d2ec78
Balraj <balraj.johal@phntms.com>
2023-08-07 22:29:40 +0100

    Test fetching all pro work images on init render

--- src/components/UI/ProfessionalWorkPage/ProfessionalEntryCard/index.tsx
+++ src/components/UI/ProfessionalWorkPage/ProfessionalEntryCard/index.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useEffect, useMemo } from "react";
+
 import { getImageURL } from "@/utils/contentful";
 import { CursorType } from "@/types/cursor";
 import { ContentfulResponse, ProfessionalContentEntry } from "@/types/content";
@@ -20,9 +22,30 @@ interface Props {
 
 const ProfessionalEntryCard = ({ activeIndex, content }: Props) => {
   const visibleEntry = content[activeIndex]?.fields as ProfessionalContentEntry;
+
+  // build object of promises for all images
+  const imageFetches = useMemo(() => {
+    const promises = [];
+    for (const entry of content) {
+      const fields = entry.fields as ProfessionalContentEntry;
+      const imageURL = getImageURL(fields.image);
+      console.log("adding URL:", imageURL);
+      promises.push(fetch(imageURL));
+    }
+    return promises;
+  }, [content]);
+
+  // on render, fetch all these images to ensure they're
+  // loaded when the user reaches the parent panel
+  useEffect(() => {
+    Promise.all(imageFetches).then((results: unknown) => {
+      console.log(results);
+    });
+  }, [imageFetches]);
+
+  // destructure content
   if (!visibleEntry) return null;
   const { image, title, slug, oneLiner } = visibleEntry;
-
   const imageURL = getImageURL(image);
   const imageAlt = `Image of ${title}`;
   const linkHref = `/work/${slug}`;
