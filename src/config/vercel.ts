bad4fab9c8dbd067815ce524c3f26a9ef77a7655
Balraj <balraj.johal@phntms.com>
2023-08-14 21:36:57 +0100

    Fix client side access for env variable

--- src/config/vercel.ts
+++ src/config/vercel.ts
@@ -1,4 +1,7 @@
-const DEPLOYED_URL = process.env.NEXT_PUBLIC_URL ?? process.env.VERCEL_ENV;
+const DEPLOYED_URL =
+  process.env.NEXT_PUBLIC_URL ??
+  process.env.VERCEL_ENV ??
+  process.env.NEXT_PUBLIC_VERCEL_URL;
 
 export const FULL_DEPLOYED_URL = DEPLOYED_URL
   ? `https://${DEPLOYED_URL}`
