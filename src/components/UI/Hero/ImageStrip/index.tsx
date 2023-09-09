6848820ee0dc7e1928d005cbc05676980a818f44
Balraj Johal <balrajsjohal@gmail.com>
2023-09-09 14:54:19 +0100

    Refactor logic to local hook

--- src/components/UI/Hero/ImageStrip/index.tsx
+++ src/components/UI/Hero/ImageStrip/index.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 import Image from "next/image";
-import useWindowSize from "@buildinams/use-window-size";
 
 import { ImageInfo } from "@/types/content";
 
+import { useImageStrip } from "./useImageStrip";
 import { ImageContainer, ImageStripWrapper } from "./styles";
 
 interface Props {
@@ -14,32 +12,9 @@ interface Props {
 }
 
 const ImageStrip = ({ images }: Props) => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [imageWidth, setImageWidth] = useState(0);
-  const { width } = useWindowSize();
-
-  const count = images.length;
-
-  useEffect(() => {
-    const base = width / count;
-    setImageWidth(base * 1.5);
-  }, [count, width]);
-
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      const { clientX } = e;
-      const fr = width / count;
-      setActiveIndex(Math.floor(clientX / fr));
-    };
-
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => window.removeEventListener("mousemove", handleMouseMove);
-  }, [count, width]);
-
-  const containerLeftPositions = images.map((_, i) => {
-    const safeArea = width - imageWidth;
-    const offset = safeArea / (count - 1);
-    return `${offset * i}px`;
+  const { activeIndex, containerLeftPositions, imageWidth } = useImageStrip({
+    count: images.length,
+    scale: 2,
   });
 
   return (
