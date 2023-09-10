3053f6cd63a6dcad13362c5927960c6c51e037e0
Balraj Johal <balraj-johal@users.noreply.github.com>
2023-09-10 23:48:17 +0100

    Add 404 handler to site root too (#59)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import TransitionLink from "@/components/UI/TransitionLink";
+import FullHeight from "@/components/UI/FullHeight";
+
+export default function NotFound() {
+  return (
+    <FullHeight>
+      <h1>NAH</h1>
+      <TransitionLink href="/">Back Home</TransitionLink>
+    </FullHeight>
+  );
+}
