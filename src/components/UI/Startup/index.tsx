cc4ad23aebafadce7a672867aafc3a82c5790c33
Balraj Johal <balrajsjohal@gmail.com>
2023-09-10 23:47:50 +0100

    Test more

--- src/components/UI/Startup/index.tsx
+++ src/components/UI/Startup/index.tsx
@@ -1,6 +1,8 @@
-// import { useRef, useState, AnimationEvent } from "react";
+"use client";
 
-// import useScrollLock from "@/hooks/useScrollLock";
+import { useRef, useState, AnimationEvent } from "react";
+
+import useScrollLock from "@/hooks/useScrollLock";
 
 import { StartupFill, StartupWrapper } from "./styles";
 
@@ -8,18 +10,24 @@ import { StartupFill, StartupWrapper } from "./styles";
 export const STARTUP_ANIM_DURATION = 0.8;
 
 const Startup = () => {
-  // const [animating, setAnimating] = useState(true);
+  const [animating, setAnimating] = useState(true);
+  const [stageOneDone, setStageOneDone] = useState(false);
+  const fillRef = useRef<HTMLDivElement>(null);
 
-  // useScrollLock(animating);
-  console.log("mount");
+  useScrollLock(animating);
+
+  console.log("here");
 
   return (
     <StartupWrapper aria-hidden>
       <StartupFill
-      // onAnimationEnd={(e: AnimationEvent) => {
-      //   // setAnimating(false);
-      //   console.log(e);
-      // }}
+        ref={fillRef}
+        stageOneDone={stageOneDone}
+        onAnimationEnd={(e: AnimationEvent) => {
+          // css anim complete
+          if (e.animationName.includes("growOut")) return setStageOneDone(true);
+          setAnimating(false);
+        }}
       />
     </StartupWrapper>
   );
