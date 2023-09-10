492555317d40f7e25cdb592768e9a8935441ef39
Balraj Johal <balrajsjohal@gmail.com>
2023-09-10 18:35:16 +0100

    Setup bg animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
         <RootBody>
           <Main>
             <Header />
-
             <Suspense>{children}</Suspense>
           </Main>
           <CustomCursorWindow />
