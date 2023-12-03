ed54181113dc36533c7002796fa2bacefa2f4b1f
Balraj Johal <balrajsjohal@gmail.com>
2023-12-03 17:07:20 +0000

    Tidy

--- src/components/UI/RichTextBlock/RTText/index.tsx
+++ src/components/UI/RichTextBlock/RTText/index.tsx
@@ -1,28 +1,23 @@
 import { Text } from "@contentful/rich-text-types";
 
-import { RTTextSpan } from "../RTText/styles";
-
 interface Props {
   content: Text;
 }
 
 const mappedMarks = (content: Text) => content.marks.map((mark) => mark.type);
 
+const getMarkClassNames = (marks: string[]) => {
+  let classNames = "";
+  if (marks.includes("code")) classNames += "code ";
+  if (marks.includes("bold")) classNames += "bold ";
+  return classNames;
+};
+
 const RTText = ({ content }: Props) => {
   const marks = mappedMarks(content);
 
   if (content.value === "") return null;
-  return (
-    <RTTextSpan
-      className={`
-      ${marks.includes("code") ? "code" : ""}
-    `}
-      // isCode={marks.includes("code")}
-      isBold={marks.includes("bold")}
-    >
-      {content.value}
-    </RTTextSpan>
-  );
+  return <span className={getMarkClassNames(marks)}>{content.value}</span>;
 };
 
 export default RTText;
