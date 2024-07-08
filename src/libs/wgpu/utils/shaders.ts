a2c00c313ae4567890fcf4bc512a2b0ceb408e2a
Balraj <balraj.johal@phntms.com>
2024-07-08 22:27:20 +0100

    Improve shader compilation logging

--- src/libs/wgpu/utils/shaders.ts
+++ src/libs/wgpu/utils/shaders.ts
@@ -1,15 +1,22 @@
 export async function checkShaderModuleCompilation(module: GPUShaderModule) {
   const info = await module.getCompilationInfo();
-  let errored = false;
 
+  // check compilation info for any error messages
+  let errored = false;
   if (info.messages.length) {
     for (const message of info.messages) {
       if (message.type === "error") errored = true;
     }
   }
 
+  // if any, log errors
   if (errored) {
-    console.error("Shader compilation failed.");
+    return console.error("Shader compilation failed, ", info.messages);
+  }
+
+  // otherwise log success
+  if (module.label) {
+    console.log(`Shader compilation succeeded for ${module.label}`);
   } else {
     console.log("Shader compilation succeeded.");
   }
