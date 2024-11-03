12364c7d5fc676607f85b47bc6990557f0811803
Balraj <balraj.johal@phntms.com>
2024-11-03 17:08:27 +0000

    Render quad from compute shader

--- src/app/labs/wgpu/compute/index.ts
+++ src/app/labs/wgpu/compute/index.ts
@@ -1,9 +1,17 @@
-import { alignTo, createShaderModule, FLOAT_LENGTH_BYTES } from "@/libs/wgpu";
+import {
+  alignTo,
+  Camera,
+  createShaderModule,
+  FLOAT_LENGTH_BYTES,
+  WebGpuApi,
+} from "@/libs/wgpu";
 import { WebGPUInstance, WebGPUInstanceProperties } from "@/libs/wgpu";
 
-import { getComputeShader } from "./shaders";
+import { getComputeShader, getRenderShader } from "./shaders";
 
 type WebGPUExplorationProperties = WebGPUInstanceProperties;
+
+const DEPTH_TEXTURE_FORMAT: GPUTextureFormat = "depth24plus-stencil8";
 
 const COMPUTE_INPUT = new Float32Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
@@ -17,24 +25,107 @@ const COMPUTE_INPUT = new Float32Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 const NUMBER_VERTICES_MAX = 4;
 const NUMBER_INDICES_MAX = 6;
 
-const INDICES = [
+const INDICES = new Uint16Array([
   0,
   1,
-  3, // Tri 1
-  1,
-  3,
   2, // Tri 2
-];
+  0,
+  2,
+  3, // Tri 1
+]);
 
 export default class WebGPUExplorationCompute extends WebGPUInstance {
+  private camera;
+
   constructor(properties: WebGPUExplorationProperties) {
     super(properties);
+
+    this.camera = new Camera({
+      canvas: this.canvas,
+      initialPosition: [0, 0, 5],
+      nearPlane: 0.01,
+      farPlane: 10,
+    });
+  }
+
+  private createVertexState(shaderModule: GPUShaderModule): GPUVertexState {
+    const positionAttribute: GPUVertexAttribute = {
+      format: "float32x3",
+      offset: 0,
+      shaderLocation: 0,
+    };
+
+    const bufferLayout: GPUVertexBufferLayout = {
+      arrayStride: 3 * FLOAT_LENGTH_BYTES, // 1 vec3<f32>
+      attributes: [positionAttribute],
+    };
+
+    return {
+      module: shaderModule,
+      entryPoint: "vertex_main", // Name of the vertex module defined in shader
+      buffers: [bufferLayout],
+    };
+  }
+
+  private createFragmentState(shaderModule: GPUShaderModule): GPUFragmentState {
+    const target: GPUColorTargetState = {
+      format: this.preferredFormat,
+    };
+
+    return {
+      module: shaderModule,
+      entryPoint: "fragment_main", // Name of the fragment module defined in shader
+      targets: [target], // NOTE: here's where MRT would be configured I think
+    };
+  }
+
+  private createRenderPassDescription(
+    depthStencilTexture: GPUTexture,
+  ): GPURenderPassDescriptor {
+    const colorAttachment: GPURenderPassColorAttachment = {
+      // view will be updated to the current render target each frame
+      view: null as unknown as GPUTextureView,
+      loadOp: "clear",
+      clearValue: [0.9, 0.3, 0.3, 1],
+      storeOp: "store",
+    };
+    const depthStencilAttachment: GPURenderPassDepthStencilAttachment = {
+      // view will be set to the current render target each frame
+      view: depthStencilTexture.createView(),
+      depthLoadOp: "clear",
+      depthClearValue: 1.0,
+      depthStoreOp: "store",
+      stencilLoadOp: "clear",
+      stencilClearValue: 0,
+      stencilStoreOp: "store",
+    };
+
+    return {
+      label: "render pass",
+      colorAttachments: [colorAttachment],
+      depthStencilAttachment,
+    };
+  }
+
+  private createDepthStencilTexture(api: WebGpuApi) {
+    if (!this.api) throw new Error("No WebGPU API ready");
+
+    return this.api.device.createTexture({
+      size: {
+        width: this.currentCanvasDimensions.width,
+        height: this.currentCanvasDimensions.height,
+        depthOrArrayLayers: 1,
+      },
+      format: DEPTH_TEXTURE_FORMAT,
+      usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    });
   }
 
   async start() {
     await this.initializeContext({ usage: GPUTextureUsage.RENDER_ATTACHMENT });
     if (!this.api) throw new Error("No WebGPU API ready");
 
+    /** -- COMPUTE -- */
     const computeShader = getComputeShader([64, 1, 1]);
 
     const computeShaderModule = await createShaderModule(this.api, {
@@ -51,7 +142,7 @@ export default class WebGPUExplorationCompute extends WebGPUInstance {
       },
     });
 
-    const computeInput = this.api.device.createBuffer({
+    const computeInputBuffer = this.api.device.createBuffer({
       label: "input data buffer",
       size: alignTo(COMPUTE_INPUT.byteLength, FLOAT_LENGTH_BYTES),
       usage:
@@ -59,31 +150,30 @@ export default class WebGPUExplorationCompute extends WebGPUInstance {
         GPUBufferUsage.COPY_SRC |
         GPUBufferUsage.COPY_DST,
     });
-    this.api.device.queue.writeBuffer(computeInput, 0, COMPUTE_INPUT);
+    this.api.device.queue.writeBuffer(computeInputBuffer, 0, COMPUTE_INPUT);
 
-    const outputLength =
-      (NUMBER_VERTICES_MAX * 3 + // 3 floats for vector
-        NUMBER_INDICES_MAX) *
-      FLOAT_LENGTH_BYTES;
+    const vertexLength = NUMBER_VERTICES_MAX * 3 * FLOAT_LENGTH_BYTES;
+    const indexLength = NUMBER_INDICES_MAX * FLOAT_LENGTH_BYTES;
+    const computeOutputLength = vertexLength + indexLength;
 
-    const computeOutput = this.api.device.createBuffer({
+    const computeOutputBuffer = this.api.device.createBuffer({
       label: "output data buffer",
-      size: alignTo(outputLength, FLOAT_LENGTH_BYTES),
+      size: alignTo(computeOutputLength, FLOAT_LENGTH_BYTES),
       usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
     });
 
     const computeBindGroup = this.api.device.createBindGroup({
       layout: computePipeline.getBindGroupLayout(0),
       entries: [
-        { binding: 0, resource: { buffer: computeInput } },
-        { binding: 1, resource: { buffer: computeOutput } },
+        { binding: 0, resource: { buffer: computeInputBuffer } },
+        { binding: 1, resource: { buffer: computeOutputBuffer } },
       ],
     });
 
-    const computeResults = this.api.device.createBuffer({
-      label: "read results data buffer",
-      size: alignTo(outputLength, FLOAT_LENGTH_BYTES),
-      usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+    const computeResultsBuffer = this.api.device.createBuffer({
+      label: "compute results data buffer",
+      size: alignTo(computeOutputLength, FLOAT_LENGTH_BYTES),
+      usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ,
     });
 
     const performComputePass = (commandEncoder: GPUCommandEncoder) => {
@@ -98,12 +188,126 @@ export default class WebGPUExplorationCompute extends WebGPUInstance {
       computePass.end();
 
       commandEncoder.copyBufferToBuffer(
-        computeOutput,
+        computeOutputBuffer,
         0,
-        computeResults,
+        computeResultsBuffer,
         0,
-        computeResults.size,
+        computeResultsBuffer.size,
       );
+    };
+
+    /** -- render -- */
+    const renderShader = getRenderShader();
+    const renderShaderModule = await createShaderModule(this.api, {
+      label: "render",
+      code: renderShader,
+    });
+
+    const vertexState = this.createVertexState(renderShaderModule);
+    const fragmentState = this.createFragmentState(renderShaderModule);
+
+    let depthStencilTexture = this.createDepthStencilTexture(this.api);
+    let renderPassDescription =
+      this.createRenderPassDescription(depthStencilTexture);
+
+    const meshVertexBuffer = this.api.device.createBuffer({
+      label: "mesh vertex buffer",
+      size: vertexLength,
+      usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+
+    const meshIndexBuffer = this.api.device.createBuffer({
+      label: "mesh index buffer",
+      size: indexLength,
+      usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST,
+    });
+    this.api.device.queue.writeBuffer(meshIndexBuffer, 0, INDICES);
+
+    const bindGroupLayout = this.api.device.createBindGroupLayout({
+      entries: [
+        {
+          binding: 0,
+          visibility: GPUShaderStage.VERTEX,
+          buffer: { type: "uniform" },
+        },
+      ],
+    });
+    const renderPipelineLayout = this.api.device.createPipelineLayout({
+      bindGroupLayouts: [bindGroupLayout],
+    });
+    const renderPipeline = await this.api.device.createRenderPipelineAsync({
+      layout: renderPipelineLayout,
+      vertex: vertexState,
+      fragment: fragmentState,
+      depthStencil: {
+        format: DEPTH_TEXTURE_FORMAT,
+        depthWriteEnabled: true,
+        depthCompare: "less",
+      },
+    });
+
+    const viewParametersBuffer = this.camera.createBuffer(this.api);
+    const viewParameterBindGroup = this.api.device.createBindGroup({
+      layout: bindGroupLayout,
+      entries: [{ binding: 0, resource: { buffer: viewParametersBuffer } }],
+    });
+
+    const performRenderPass = (commandEncoder: GPUCommandEncoder) => {
+      if (!this.api) throw new Error("No WebGPU API ready");
+
+      if (this.hasResized()) {
+        this.setupCanvas();
+
+        depthStencilTexture?.destroy();
+
+        depthStencilTexture = this.createDepthStencilTexture(this.api);
+        renderPassDescription =
+          this.createRenderPassDescription(depthStencilTexture);
+
+        this.configureContext({
+          usage: GPUTextureUsage.RENDER_ATTACHMENT,
+        });
+      }
+
+      const colorAttachments = [...renderPassDescription.colorAttachments];
+      const firstColorAttachment = colorAttachments[0];
+
+      if (firstColorAttachment != null) {
+        firstColorAttachment.view = this.api.context
+          .getCurrentTexture()
+          .createView();
+      }
+
+      const cameraBuffer = this.camera.getUpdatedBuffer(this.api);
+      commandEncoder.copyBufferToBuffer(
+        cameraBuffer,
+        0,
+        viewParametersBuffer,
+        0,
+        16 * FLOAT_LENGTH_BYTES,
+      );
+
+      commandEncoder.copyBufferToBuffer(
+        computeOutputBuffer,
+        0,
+        meshVertexBuffer,
+        0,
+        // vertexLength,
+        vertexLength,
+      );
+
+      const renderPass = commandEncoder.beginRenderPass(renderPassDescription);
+      renderPass.setPipeline(renderPipeline);
+      // TODO: is this doin shit?
+      renderPass.setBindGroup(0, viewParameterBindGroup);
+
+      // TODO: get data buffer from compute shader
+      renderPass.setVertexBuffer(0, meshVertexBuffer);
+      renderPass.setIndexBuffer(meshIndexBuffer, "uint16");
+      renderPass.drawIndexed(6);
+      // renderPass.draw(4, 1, 0, 0);
+
+      renderPass.end();
     };
 
     const render = async () => {
@@ -114,17 +318,20 @@ export default class WebGPUExplorationCompute extends WebGPUInstance {
 
       performComputePass(commandEncoder);
 
+      await computeResultsBuffer.mapAsync(GPUMapMode.READ);
+      const range = computeResultsBuffer.getMappedRange();
+      const result = new Float32Array(range).slice(0, 12);
+      computeResultsBuffer.unmap();
+      console.log(result);
+
+      // TODO: can we go straight buffer to buffer here?
+      performRenderPass(commandEncoder);
+
       // submit commands to GPU to render
       this.api.device.queue.submit([commandEncoder.finish()]);
 
       // request render of next frame
       requestAnimationFrame(render);
-
-      await computeResults.mapAsync(GPUMapMode.READ);
-      const range = computeResults.getMappedRange();
-      const result = new Float32Array(range).slice(0, 12);
-      computeResults.unmap();
-      console.log(result);
     };
 
     requestAnimationFrame(render);
